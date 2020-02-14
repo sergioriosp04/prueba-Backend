f9f237e478176b97d06bfe91d419091d49bed5d0
Sergio Rios <sergioriosp04@gmail.com>
2020-02-13 19:28:54 -0500

    se avanzo con el registro de usuario, continuare en otro pc

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,25 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     //
-    public function registro(){
+    public function registro(Request $request){
+
+        $json = $request->input('json', null);
+        $params = json_encode($json);
+        $params_array = json_encode($json, true);
+
+        if(!empty($params_array)){
+            //limpiar datos
+            $params_array = array_map('trim', $params_array);
+
+            $validator = \Validator::make($params_array, [
+                'nombre' => 'required',
+                'email' => 'required|email|unique:users'
+            ]);
+        }else{
+
+        }
+
+
         return 'registro cliente';
     }
 }
